357416ce39d60cd5b915850633b90fdea1f0404c
yatniel <yatniel@qq.com>
2020-04-27 20:15:54 +0800

    test

--- project/model/user/index.go
+++ project/model/user/index.go
@@ -9,7 +9,7 @@ func init() {
 
 	ark.Register("user.Serial", ark.Method{
 		Name: "生成序列", Desc: "生成序列",
-		Action: func(ctx *ark.Service) (Map, *Res) {
+		Action: func(ctx *ark.Program) (Map, *Res) {
 			serial := ark.CacheSerial("hahha", 0, 1)
 			return Map{
 				"serial": serial,
@@ -20,7 +20,7 @@ func init() {
 
 	ark.Register("user.Test", ark.Method{
 		Name: "测试", Desc: "测试",
-		Action: func(ctx *ark.Service) Map {
+		Action: func(ctx *ark.Program) Map {
 
 			bll := ctx.Logic("test", Map{
 				"msg": "msg from setting",
@@ -32,7 +32,7 @@ func init() {
 
 	ark.Register("user.Notify", ark.Method{
 		Name: "用户通知", Desc: "用户通知",
-		Action: func(ctx *ark.Service) *Res {
+		Action: func(ctx *ark.Program) *Res {
 			ark.Debug("收到用户通知事件", ctx.Value)
 
 			return ark.OK
@@ -41,7 +41,7 @@ func init() {
 
 	ark.Register("user.Update", ark.Method{
 		Name: "用户更新", Desc: "用户更新",
-		Action: func(ctx *ark.Service) *Res {
+		Action: func(ctx *ark.Program) *Res {
 			ark.Debug("收到用户更新队列", ctx.Value)
 			return ark.OK
 		},
